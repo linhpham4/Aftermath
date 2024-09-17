4633d3ddfe9e6e96253fd6d9016ec04f80ee5c3d
Linh Pham <linh.p44@gmail.com>
2024-09-17 16:57:13 -0700

    Create seed file for items table

---
+++ aftermath-server/seeds/03_items.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  await knex("items").del();
+  await knex("items").insert([
+    {
+      id: 1,
+      bill_id: 1,
+      description: "Whiskey Old Fashioned",
+      quantity: 1,
+      total: 9.03,
+    },
+    {
+      id: 2,
+      bill_id: 1,
+      description: "Loaded Nachos",
+      quantity: 1,
+      total: 18.06,
+    },
+    {
+      id: 3,
+      bill_id: 1,
+      description: "Soup of the Day Cup",
+      quantity: 1,
+      total: 6.01,
+    },
+  ]);
+}
