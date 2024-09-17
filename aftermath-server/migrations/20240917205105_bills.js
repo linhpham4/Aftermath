ff18175aa23c9fedc50571eaa6fd35931f6a8b77
Linh Pham <linh.p44@gmail.com>
2024-09-17 15:26:06 -0700

    Create migration file for bills table

---
+++ aftermath-server/migrations/20240917205105_bills.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("bills", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("host_id")
+      .unsigned()
+      .notNullable()
+      .references("people.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("restaurant");
+    table.decimal("subtotal", 8, 2).notNullable();
+    table.decimal("tax", 8, 2).notNullable();
+    table.decimal("tip", 8, 2).notNullable();
+    table.decimal("total", 8, 2).notNullable();
+    table.string("image_url").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("bills");
+}
