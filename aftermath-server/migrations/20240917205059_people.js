6eca16dc1f6f0b562559d19f460d82405f1e23b4
Linh Pham <linh.p44@gmail.com>
2024-09-17 15:20:34 -0700

    Add "created_at" and "updated_at" timestamp fields to people migration file

--- aftermath-server/migrations/20240917205059_people.js
+++ aftermath-server/migrations/20240917205059_people.js
@@ -8,6 +8,10 @@ export function up(knex) {
     table.string("name").notNullable();
     table.string("email").unique();
     table.string("password");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
   });
 }
 
