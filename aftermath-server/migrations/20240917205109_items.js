104652eb3d43e090f6860133c5223890562318dc
Linh Pham <linh.p44@gmail.com>
2024-09-17 15:27:43 -0700

    Create migration file for items table

---
+++ aftermath-server/migrations/20240917205109_items.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("items", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("bill_id")
+      .unsigned()
+      .notNullable()
+      .references("bills.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("description").notNullable();
+    table.integer("quantity").notNullable();
+    table.decimal("total", 8, 2).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable("items");
+}
