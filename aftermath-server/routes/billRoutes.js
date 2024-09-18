1232032a227eb0e6ca8aec870d8524bd30ce3a2b
Linh Pham <linh.p44@gmail.com>
2024-09-18 12:34:40 -0700

    Add all GET routes

--- aftermath-server/routes/billRoutes.js
+++ aftermath-server/routes/billRoutes.js
@@ -1,14 +1,46 @@
 import express from "express";
 import * as billController from "../controllers/billController.js";
+import * as itemController from "../controllers/itemController.js";
+import * as transactionController from "../controllers/transactionController.js";
+import * as peopleController from "../controllers/peopleController.js";
 
 const router = express.Router();
-
+// Bills
 router
     .route("/bills")
-    .post(billController.saveBill);
+    .post(billController.saveBill)
+    .get(billController.getAllBills);
 
 router
     .route("/bills/:billId")
-    .get(billController.getBill);
+    .get(billController.getBill)
+
+// Items
+router
+    .route("/items")
+    .get(itemController.getAllItems);
+
+router
+    .route("/items/:itemId")
+    .get(itemController.getItem);
+
+// Transactions
+router
+    .route("/transactions")
+    .get(transactionController.getAllTransactions);
+
+router
+    .route("/transactions/:transactionId")
+    .get(transactionController.getTransaction);
+
+
+// People
+router
+    .route("/people")
+    .get(peopleController.getAllPeople);
+
+router
+    .route("/people/:personId")
+    .get(peopleController.getPerson);
 
 export default router;
