ac745c5fa1a72bdda673ebbd2a4348f972c4c6d3
Linh Pham <linh.p44@gmail.com>
2024-09-18 14:11:26 -0700

    Add validation for addTransaction to check for missing values, that item_id, payment_to_id, payment_from_id exists in their respective tables, total is a number, and settled is a boolean

--- aftermath-server/controllers/transactionController.js
+++ aftermath-server/controllers/transactionController.js
@@ -46,6 +46,43 @@ const getTransaction = async (req, res) => {
 // Add a transaction
 const addTransaction = async (req, res) => {
   try {
+     // Checks that request body contains all required data
+     if (
+      !req.body.item_id ||
+      !req.body.payment_to_id ||
+      !req.body.payment_from_id ||
+      !req.body.total ||
+      !req.body.settled
+    ) {
+      return res.status(400).json("Please provide all required transaction information");
+    }
+
+    // Checks that item with id matching bill_id from the request exists
+    const items = await knex("items");
+    if (!items.find((item) => item.id === req.body.item_id)) {
+      return res.status(400).json(`Item with ID ${req.body.item_id} not found`);
+    }
+
+    // Checks that people with id matching payment_to_id and payment_from_id from the request exists
+    const people = await knex("people");
+    if (!people.find((person) => person.id === req.body.payment_to_id)) {
+      return res.status(400).json(`Person with ID ${req.body.payment_to_id} not found`);
+    }
+
+    if (!people.find((person) => person.id === req.body.payment_from_id)) {
+      return res.status(400).json(`Person with ID ${req.body.payment_from_id} not found`);
+    }
+
+    // Checks if the value of total is a number
+    if (typeof req.body.total !== "number") {
+      return res.status(400).json("Total must be a number");
+    }
+
+    // Checks if the value of settled is a boolean
+    if (typeof req.body.settled !== "boolean") {
+      return res.status(400).json("Value of settled must be a boolean");
+    }
+
     const updatedTransactionsList = await knex("transactions").insert(req.body);
     const newTransactionId = updatedTransactionsList[0];
     const newTransaction = await knex("transactions").where({ id: newTransactionId }).select();
