1e438ea087b95d918bff3d537a73b4f4326e5efa
Linh Pham <linh.p44@gmail.com>
2024-09-18 13:21:22 -0700

    Add validation for addPerson to check that name property is not empty, email is in the right format if it exists, and that it doesn't already exist in people table

--- aftermath-server/controllers/peopleController.js
+++ aftermath-server/controllers/peopleController.js
@@ -1,5 +1,6 @@
 import initKnex from "knex";
 import configuration from "../knexfile.js";
+import validator from "validator";
 
 const knex = initKnex(configuration);
 
@@ -36,15 +37,32 @@ const getPerson = async (req, res) => {
 
 //---------------------------------------------------------------------------------------------
 
-// Add a person
+// Add an item
 const addPerson = async (req, res) => {
   try {
+    // Checks that name property is defined
+    if (!req.body.name) {
+      return res.status(400).json("Person must be named");
+    }
+
+    // Checks that if email has a value, it is in the correct format
+    if (req.body.email && !validator.isEmail(req.body.email)) {
+      return res.status(400).json(`Invalid email address`);
+    }
+
+    // Checks email does not already exist in people table
+    const peopleList = await knex("people");
+    peopleList.map((person) => {
+      if(person.email === req.body.email) {
+        return res.status(400).json(`User with email ${req.body.email} already exists`);
+      }
+    });
 
     const updatedPeopleList = await knex("people").insert(req.body);
     const newPersonId = updatedPeopleList[0];
-    const newPerson = await knex("people").where({ id: newPersonId }).select()
+    const newPerson = await knex("people").where({ id: newPersonId }).select();
 
-    res.status(200).json(newPerson);
+    res.status(200).json(newPerson[0]);
   } catch (error) {
     console.log(error);
   }
