80535e02d8fae542591190e9934f04434864dfec
Linh Pham <linh.p44@gmail.com>
2024-09-18 00:49:37 -0700

    Create saveBill function to save response from Veryfi API into the bills table

--- aftermath-server/controllers/billController.js
+++ aftermath-server/controllers/billController.js
@@ -12,33 +12,69 @@ const USERNAME = process.env.USERNAME;
 const API_KEY = process.env.API_KEY;
 
 // Upload image to Veryfi API bucket to be processed into json data
-const convertBill = async (req, res) => {
+const convertBill = async (imageUrl) => {
   try {
-/* 
-  ┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-  │     change from static file to file user upload later!                                                              │
-  └─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
- */
     // Create new form to append image file to
     const form = new FormData();
-    form.append("file", fs.createReadStream("./public/images/bill1.png"));
+    form.append("file", fs.createReadStream(`${imageUrl}`));
 
     // POST request to Veryfi API
-    const response = await axios.post(BASE_URL, form,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Accept: "application/json",
-          "CLIENT-ID": `${CLIENT_ID}`,
-          AUTHORIZATION: `apikey ${USERNAME}:${API_KEY}`,
-        },
-      }
-    );
+    const response = await axios.post(BASE_URL, form, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+        "CLIENT-ID": `${CLIENT_ID}`,
+        AUTHORIZATION: `apikey ${USERNAME}:${API_KEY}`,
+      },
+    });
 
-    res.status(200).json(response.data.img_url)
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-export { convertBill };
+//---------------------------------------------------------------------------------------------
+
+// Call convertBill and save the response to database
+const saveBill = async (req, res) => {
+  try {
+    const imageUrl = req.body.url;
+    const bill = await convertBill(imageUrl);
+
+    // If there are any properties that are null, set the value to 0 instead
+    for (const property in bill) {
+      if (bill[property] === null) {
+        bill[property] = 0;
+      }
+    }
+
+    /* 
+      ┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+      │ host_id will change dynamically later when there are multiple users                                                         │
+      └─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+     */
+
+    // Add the necessary data from Veryfi API response to database
+    const newBill = await knex("bills").insert({
+      host_id: 1,
+      restaurant: bill.vendor.name,
+      subtotal: bill.subtotal,
+      tax: bill.tax,
+      tip: bill.tip,
+      total: bill.total,
+      image_url: bill.img_url,
+    });
+
+    const newBillId = newBill[0];
+    const newBillData = await knex("bills").where({ id: newBillId }).select();
+
+    res.status(201).json(newBillData[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//---------------------------------------------------------------------------------------------
+
+export { saveBill };
