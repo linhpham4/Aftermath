7ef8fd5918ac427112aac1008457625c56198b4b
Linh Pham <linh.p44@gmail.com>
2024-09-18 21:00:15 -0700

    Set up basic routes

--- aftermath-client/src/App.jsx
+++ aftermath-client/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit/:id" element={<EditBillPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
