7860111d3e0c0121d3bbe1331ae78326c78f6403
Linh Pham <linh.p44@gmail.com>
2024-09-19 09:43:46 -0700

    Build static HTML and SCSS styling for HomePage

--- aftermath-client/src/pages/HomePage/HomePage.jsx
+++ aftermath-client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,44 @@
 import "./HomePage.scss";
+import logo from "../../assets/logo/aftermath_logo.svg";
 
 const HomePage = () => {
   return (
-    <>
-      <h1>Welcome to the home page!</h1>
-    </>
+    <main className="home">
+      <div className="home__container">
+        <img className="home__logo" src={logo} alt="aftermath logo" />
+
+        <div className="home__text-container">
+          <h2 className="home__body">
+            1 &emsp; Add friends to the bill &emsp; <br />
+            2 &emsp;Assign items &emsp; <br />
+            3 &emsp;Don't forget about tip &emsp; <br />
+            4 &emsp;Enjoy not doing math! &emsp; <br />
+          </h2>
+        </div>
+
+        <form
+          className="home__form"
+          id="bill"
+          method="post"
+          encType="multipart/form-data"
+        >
+          <label className="home__label" htmlFor="bill">
+            Upload a picture of your bill to get started!
+          </label>
+          <input
+            className="home__input"
+            type="file"
+            id="bill"
+            name="bill"
+            accept="image/*"
+          ></input>
+        </form>
+      </div>
+
+      <button className="home__button" form="bill">
+        Next
+      </button>
+    </main>
   );
 };
 
