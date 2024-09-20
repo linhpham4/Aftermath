2e7ca70d41d3551b0a278ba9a394d0753fd65f10
Linh Pham <linh.p44@gmail.com>
2024-09-19 17:04:03 -0700

    Configure multer to send incoming image file to public/images folder

--- aftermath-server/routes/billRoutes.js
+++ aftermath-server/routes/billRoutes.js
@@ -3,12 +3,26 @@ import * as billController from "../controllers/billController.js";
 import * as itemController from "../controllers/itemController.js";
 import * as transactionController from "../controllers/transactionController.js";
 import * as peopleController from "../controllers/peopleController.js";
+import multer from "multer";
 
 const router = express.Router();
+
+// Store image uploaded from front end into public/images folder
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images")
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`)
+    }
+});
+
+const upload = multer({storage: storage});
+
 // Bills
 router
     .route("/bills")
-    .post(billController.saveBill)
+    .post(upload.any(), billController.saveBill)
     .get(billController.getAllBills);
 
 router
