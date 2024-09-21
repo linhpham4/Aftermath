7c241bc03b42193a5cd7e15ceac4f9a33986b20c
Linh Pham <linh.p44@gmail.com>
2024-09-21 12:12:06 -0700

    Add do...while loop to ensure the same color is not repeated

--- aftermath-client/src/components/EditPersonNameModal/EditPersonNameModal.jsx
+++ aftermath-client/src/components/EditPersonNameModal/EditPersonNameModal.jsx
@@ -5,18 +5,13 @@ import axios from "axios";
 const EditPersonNameModal = ({ open, close, setName, setPersonId, setColor }) => {
   const [text, setText] = useState("");
   const [showError, setShowError] = useState("");
+  const [usedColors, setUsedColors] = useState([]);
   const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-  // Clears any text in input upon modal opening
+  // Clears any text in input field upon modal opening
   useEffect(() => {
     setText("");
-  }, [open]);
-  
-  // Picks a random number (degree) for color filter for each new avatar
-  useEffect(() => {
-    const degree = Math.floor(Math.random() * 360);
-    setColor(degree);
-  },[]);
+}, [open]);
 
   // Retrieve name from input, must be filled out
   const handleChange = (event) => {
@@ -26,17 +21,29 @@ const EditPersonNameModal = ({ open, close, setName, setPersonId, setColor }) =>
     setText(event.target.value);
   };
 
+  // Picks a random number (degree) for color filter for each new avatar
+  const pickColor =()=> {
+    let degree = Math.floor(Math.random() * (320/30) + 1) * 30
+    do {
+      degree = Math.floor(Math.random() * (320/30) + 1) * 30
+    } while (usedColors.includes(degree));
+    setColor(degree);
+    setUsedColors([...usedColors, degree]);
+  }
+
   // Post request to add new person to database
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (text === "") {
       return setShowError(true);
     }
+
     const response = await axios.post(`${BASE_URL}/people`, {name: text});
 
     // Set state variables to the new person pass back to parent
     setPersonId(response.data.id);
     setName(text);
+    pickColor();
     close();
   };
 
