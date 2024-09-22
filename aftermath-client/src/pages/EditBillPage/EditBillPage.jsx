43b05fec068efd2ecf09da99105ee0617026b326
Linh Pham <linh.p44@gmail.com>
2024-09-21 20:29:01 -0700

    Dynamically render line items

--- aftermath-client/src/pages/EditBillPage/EditBillPage.jsx
+++ aftermath-client/src/pages/EditBillPage/EditBillPage.jsx
@@ -13,9 +13,9 @@ const EditBillPage = () => {
     tax: 0,
     tip: 0,
     total: 0,
-    image_url: ""
+    image_url: "",
   });
-  const [item, setItem] = useState([]);
+  const [items, setItems] = useState([]);
   const [name, setName] = useState("");
   const [personId, setPersonId] = useState(null);
   const [color, setColor] = useState(null);
@@ -26,14 +26,17 @@ const EditBillPage = () => {
   const { id } = useParams();
 
   //Get data for bill matching {id}
-  const getBill = async() => {
+  const getBill = async () => {
     try {
       const response = await axios.get(`${BASE_URL}/bills/${id}`);
       setBill(response.data);
+
+      const lineItems = response.data.line_items;
+      setItems(lineItems);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getBill();
@@ -51,27 +54,25 @@ const EditBillPage = () => {
     }));
   };
 
-
   // Update bill total when subtotal, tax, or tip changes
   const updateTotal = () => {
     setBill((prevState) => ({
       ...prevState,
-      total: Number(prevState.subtotal + prevState.tax + prevState.tip).toFixed(2)
-    }))
-  }
+      total: Number(prevState.subtotal + prevState.tax + prevState.tip).toFixed(2),
+    }));
+  };
 
   useEffect(() => {
     updateTotal();
-  }, [bill.subtotal, bill.tax, bill.tip])
+  }, [bill.subtotal, bill.tax, bill.tip]);
 
   // When new person is created, they get added to the people state variable
   useEffect(() => {
     if (personId !== null) {
-
       const newPerson = {
         id: personId,
         name: name,
-        color: color
+        color: color,
       };
 
       setPeople([...people, newPerson]);
@@ -84,7 +85,7 @@ const EditBillPage = () => {
   };
 
   if (!bill) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   return (
@@ -109,41 +110,48 @@ const EditBillPage = () => {
 
       <form className="edit__form" id="editBill">
         {/* Dynamically generated ----------------------------------------------- */}
-        <div className="edit__item">
-          <div className="edit__input-container">
-            <input
-              className="edit__input edit__input--quantity"
-              type="text"
-              id="quantity"
-              name="quantity"
-              value="1"
-            />
-            <input
-              className="edit__input edit__input--description"
-              type="text"
-              id="description"
-              name="description"
-              value="Loaded Nachos"
-            />
-            <input
-              className="edit__input edit__input--total"
-              type="text"
-              id="total"
-              name="total"
-              value="$20.21"
-            />
-          </div>
+        {items.map((item) => (
+          <div className="edit__item">
+            <div className="edit__input-container">
+              <input
+                className="edit__input edit__input--quantity"
+                type="text"
+                id="quantity"
+                name="quantity"
+                value={item.quantity}
+              />
+              <input
+                className="edit__input edit__input--description"
+                type="text"
+                id="description"
+                name="description"
+                value={item.description}
+              />
+              <input
+                className="edit__input edit__input--total"
+                type="number"
+                id="itemTotal"
+                name="itemTotal"
+                value={item.total}
+              />
+            </div>
 
-          <div className="edit__people-container">
-            <button
-              className="edit__add edit__add--item"
-              onClick={addItemPerson}
-            ></button>
-            {itemPeople.map((person) => {
-              return <div className="edit__avatar"></div>;
-            })}
+            <div className="edit__people-container">
+              <button
+                className="edit__add edit__add--item"
+                onClick={addItemPerson}
+              ></button>
+              {itemPeople.map((person) => {
+                <img
+                className="edit__avatar"
+                src={avatar}
+                alt="avatar"
+                style={{ filter: `hue-rotate(${person.color}deg)` }}
+              />
+              })}
+            </div>
           </div>
-        </div>
+        ))}
 
         <div className="edit__item edit__item--end">
           <p className="edit__text">
@@ -155,13 +163,13 @@ const EditBillPage = () => {
             <br />
           </p>
           <form className="edit__input-container edit__input-container--column">
-            <input 
+            <input
               className="edit__input edit__input--gray"
               type="number"
               id="subtotal"
               name="subtotal"
               value={bill.subtotal}
-              onChange={handleBill} 
+              onChange={handleBill}
               disabled="disabled"
             />
             <input
@@ -185,15 +193,15 @@ const EditBillPage = () => {
 
         <div className="edit__item edit__item--end">
           <p className="edit__text">Total</p>
-          <input 
-              className="edit__input edit__input--gray"
-              type="number"
-              id="total"
-              name="total"
-              value={bill.total}
-              onChange={handleBill} 
-              disabled="disabled"
-            />
+          <input
+            className="edit__input edit__input--gray"
+            type="number"
+            id="total"
+            name="total"
+            value={bill.total}
+            onChange={handleBill}
+            disabled="disabled"
+          />
         </div>
       </form>
 
