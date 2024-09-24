b22e6aaf759b8937f248ce170707d18f0c54f333
Linh Pham <linh.p44@gmail.com>
2024-09-23 21:09:52 -0700

    Update seed data

--- aftermath-server/seeds/02_bills.js
+++ aftermath-server/seeds/02_bills.js
@@ -11,21 +11,21 @@ export async function seed(knex) {
       restaurant: "Erin's Snug Pub",
       subtotal: 27.48,
       tax: 1.50,
-      tip: 4.12,
-      total: 33.10,
+      tip: 0,
+      total: 28.98,
       image_url:
-        "https://scdn.veryfi.com/receipts/5c65644d6deb80c3/3b8c7c29-b8f3-484f-8ed9-eb5ec2eb06e5/009f4413-b413-4745-b5f0-c8f50b12b190.png?Expires=1726616104&Signature=a5i79vHMcSl~xP87uqhCPu26BOxFRCW1tOKrZ1Or7P8E1AoGZrdzx-lEqPC4v5sNnGjPuduSsCf3aCHnJRwBI-W1WPAOiyV3bnIiXQvT-I~0zseJr24qWFHAyBp6pgVq0G3JBitVG3X06-LVRQqKh6rxMfauevn~N9L5CbuQLLHC9K~gOd9bI5DYzeBaRCsnjtoMCq4ibvsAg68PtBhoaa5E4eOSKc03kJc5x5kdwSfYS8Mb5qGvS8W9dNsH654qmktvJC2Ud~RhkZej53xDpebqGuJa2yHh6ePtAAZCTZpnUYZRrcLlmifTX2gUhnqqxqHaBQnaeB5482bmC0ku3g__&Key-Pair-Id=APKAJCILBXEJFZF4DCHQ",
+        "https://scdn.veryfi.com/receipts/5c65644d6deb80c3/3b8c7c29-b8f3-484f-8ed9-eb5ec2eb06e5/009f4413-b413-4745-b5f0-c8f50b12b190.png?Expires=1727150198&Signature=H20NgKbaDYY4nFtxaBE5XSuOVDXu0KLQajAQyWzodXTL1vu77nlJT9weZmd7QEG7mVr8Y4RdrCX-yj~XlqA1cXjxkf8QOh5hKj4x6dL15Rryk-SDAXg3gP01Hk2MN6flWEeiEq0mmvBVvM4jHPNucYCJwJ33l1z5UW2RBoEE2PxY6iC4UF5~U4tYTG4CYCYi4TYjsnlsn20P20AZU6MmrBEfG445wSfJzbQifMMCmIEIYJTFd-JHFfipMGjTc36JltsLHgxez6HccwCY9pjSmxhkGMRcSZgk7DoUbryGPYkYMMfPwI7e~i1jfRfyVwzRKzNGkKmS2ModQGz0eGAWvw__&Key-Pair-Id=APKAJCILBXEJFZF4DCHQ",
     },
     {
       id: 2,
       host_id: 1,
-      restaurant: "Restaurant 2",
-      subtotal: 27.48,
-      tax: 1.50,
-      tip: 4.12,
-      total: 33.10,
+      restaurant: "Good Restaurant",
+      subtotal: 31.25,
+      tax: 2.62,
+      tip: 0,
+      total: 33.87,
       image_url:
-        "https://scdn.veryfi.com/receipts/5c65644d6deb80c3/3b8c7c29-b8f3-484f-8ed9-eb5ec2eb06e5/009f4413-b413-4745-b5f0-c8f50b12b190.png?Expires=1726616104&Signature=a5i79vHMcSl~xP87uqhCPu26BOxFRCW1tOKrZ1Or7P8E1AoGZrdzx-lEqPC4v5sNnGjPuduSsCf3aCHnJRwBI-W1WPAOiyV3bnIiXQvT-I~0zseJr24qWFHAyBp6pgVq0G3JBitVG3X06-LVRQqKh6rxMfauevn~N9L5CbuQLLHC9K~gOd9bI5DYzeBaRCsnjtoMCq4ibvsAg68PtBhoaa5E4eOSKc03kJc5x5kdwSfYS8Mb5qGvS8W9dNsH654qmktvJC2Ud~RhkZej53xDpebqGuJa2yHh6ePtAAZCTZpnUYZRrcLlmifTX2gUhnqqxqHaBQnaeB5482bmC0ku3g__&Key-Pair-Id=APKAJCILBXEJFZF4DCHQ",
+        "https://scdn.veryfi.com/receipts/5c65644d6deb80c3/294028f9-d6e7-4718-8c75-7134eb17ffa0/8197679e-63f8-4f43-9ed6-4ae5c36335ca.jpg?Expires=1727150168&Signature=Kqb8KvMf7CSjmm67fYY7VJZe7j2IN~B2cDGK1WaVT70UCCvc3A6VrHG7VUVw50~QwnSGKWxjNMKwy0RWYvA9bFCcI9BvDAtwuW6oDUioLIlSlJWxRMElRBCvKeA-uHLKCOZHrnHT0AX5Ws7o~vnVAe01oeXbN4HLbVkj7j2Jia946l87ulty5UJS7ZobXY6ThrNR~meIKbNZeVg3Xtas9c~oukF7d7xXtT2nWFbsqXLWIuYBN73KRiQsriDb8ozdMpnNVenMxag1tF9CGWOif3PIsWIGpRt-wbDTTLztVD022mQsdLeaNAP7dL~WBatAbVPm5KH6t8taYNgf3Rjtmw__&Key-Pair-Id=APKAJCILBXEJFZF4DCHQ",
     }
   ]);
 };
--- aftermath-server/seeds/03_items.js
+++ aftermath-server/seeds/03_items.js
@@ -10,21 +10,21 @@ export async function seed(knex) {
       bill_id: 1,
       description: "Whiskey Old Fashioned",
       quantity: 1,
-      item_total: 9.03,
+      item_total: 7.50,
     },
     {
       id: 2,
       bill_id: 1,
       description: "Loaded Nachos",
       quantity: 1,
-      item_total: 18.06,
+      item_total: 14.99,
     },
     {
       id: 3,
       bill_id: 1,
       description: "Soup of the Day Cup",
       quantity: 1,
-      item_total: 6.01,
+      item_total: 4.99,
     },
   ]);
 }
